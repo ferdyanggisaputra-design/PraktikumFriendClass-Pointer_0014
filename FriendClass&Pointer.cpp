ecb9b5b27815c1eb055f2527a36c8d1bdde20d52
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-27 17:54:32 +0700

    Menambahkan method inputData, hitungLuas, dan hitungKeliling pada class BelahKetupat

--- FriendClass&Pointer.cpp
+++ FriendClass&Pointer.cpp
@@ -26,3 +26,43 @@ private:
     double d1, d2;
     double sisi;
 
+public:
+    void inputData() {
+        cout << "Masukkan diagonal 1 belah ketupat: ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2 belah ketupat: ";
+        cin >> d2;
+        cout << "Masukkan sisi belah ketupat: ";
+        cin >> sisi;
+    }
+
+    double hitungLuas() {
+        return 0.5 * d1 * d2;
+    }
+
+    double hitungKeliling() {
+        return 4 * sisi;
+    }
+};
+
+void tampilkanKeliling(BelahKetupat &bk) {
+    cout << "Keliling Belah Ketupat : " << bk.hitungKeliling() << endl;
+}
+
+int main() {
+    LayangLayang ObjectLayang;
+    BelahKetupat ObjectBelah;
+
+    // input data
+    ObjectLayang.inputData();
+    ObjectBelah.inputData();
+
+    cout << "\n--- Hasil Luas ---" << endl;
+    cout << "Luas Layang-Layang : " << ObjectLayang.hitungLuas() << endl;
+    cout << "Luas Belah Ketupat : " << ObjectBelah.hitungLuas() << endl;
+
+    cout << "\n--- Keliling ---" << endl;
+    tampilkanKeliling(ObjectBelah);
+
+    return 0;
+}   
